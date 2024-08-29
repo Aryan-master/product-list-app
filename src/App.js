ea228761cb26f2ad2bfcc5f1521342a72b00e0fa
Aryan Nigam <aryannigam032@gmail.com>
2024-08-29 21:14:22 +0530

    Add-app-base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import ProductDetails from './components/ProductDetails';
+import CompareProducts from './components/CompareProducts';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Sidebar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<ProductDetails />} />
+            <Route path="/compare" element={<CompareProducts />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
